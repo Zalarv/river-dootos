d2fee70b41e6c5a50a2249092fd872ece2f89125
Zalarv <156964444+Zalarv@users.noreply.github.com>
2024-08-05 22:04:08 +0000

    added whole rice

---
+++ dotconfig/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,22 @@
+return {
+
+  { "corei8/gruvbox-material.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+
+  {
+    "gruvbox-material.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
